b113895da5c0536549adf298335937b72ffefd6f
daniblopess <danielablopess@gmail.com>
2019-11-06 22:04:56 -0300

    Executei exercício function

---
+++ AULA8/script.js
@@ -0,0 +1,28 @@
+let animes = ['Death Note', 'Boku No Hero', 'Dragon Ball', 'Cavaleiros do Zodíaco', 'Naruto', 'Super Onze', 'Nazo No Kanojo'];
+let i = 3;
+
+while (i< animes.length){
+    console.log (animes [i]);
+    i ++ ;
+}
+
+
+for (let e = 0; e < animes.length; e++) {
+    console.log (animes[e]);
+}
+
+function comprarPao (dinheiro, pao){
+
+    receber o dinheiro e 
+    comprar o pao
+    return pao;
+}
+
+function
+
+var a =2;
+var b = 3;
+console.log (a+b)
+ 
+
+function construirUmaEscada ( )
